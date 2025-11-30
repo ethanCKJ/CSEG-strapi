5ad3da79d454439f439607ad2a30c948258358ab
Ethan Cheam <ethan.cheam123@gmail.com>
2025-11-30 08:24:14 +0000

    Installed module to make relations required and made eventType required on event

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,44 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.trycloudflare.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.trycloudflare.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        '*.trycloudflare.com',
+        'http://localhost:3000',
+        'http://localhost:1337',
+      ],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
