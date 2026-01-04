4bbfa62f34521b6ea646675cf20439c15a85aeb0
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-04 15:37:04 +0000

    Successfully hid unnecessary options

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -3,6 +3,8 @@ import './overrides.css'
 
 export default {
   config: {
+    tutorials: false,
+    notifications: { releases: false },
 
     locales: [
       // 'ar',
@@ -35,6 +37,37 @@ export default {
   },
   // This does not print but it may get called.
   bootstrap(app: StrapiApp) {
-    console.log('bootstrapping in src/admin/app.tsx',app);
+    console.error('bootstrapping in src/admin/app.tsx',app.store);
+
+    console.error(app.store?.getState())
+
+    const iconsToHide = ["/admin/plugins/cloud", "/admin/settings", "/admin/content-manager"];
+    /**
+     * When run, tries to hide the icon for anchor with the given href. Returns true if successful. False otherwise
+     * @param targetHref
+     */
+    const tryHideIcon = (targetHref: string) => {
+      const anchor = document.querySelector(`a[href="${targetHref}"]`);
+      if (anchor){
+        const listItem: HTMLElement | null = anchor.closest('li');
+        if (listItem){
+          listItem.style.display = 'none';
+          return true;
+        }
+      }
+      return false;
+    }
+
+    for (const href of iconsToHide){
+      if (!tryHideIcon(href)){
+        const observer = new MutationObserver(() => {
+          if (tryHideIcon(href)){
+            observer.disconnect();
+          }
+        })
+        observer.observe(document.body, {childList:true, subtree:true});
+        setTimeout(() => observer.disconnect(), 10_000);
+      }
+    }
   },
 };
