ed609cae02c23ab8f2ceec172a72fc1108079af7
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-05 17:33:04 +0000

    Trying to render the content manager table

--- src/plugins/membership-list/admin/src/pages/HomePage.tsx
+++ src/plugins/membership-list/admin/src/pages/HomePage.tsx
@@ -1,14 +1,38 @@
-import { Main } from '@strapi/design-system';
+import {Main, Typography} from '@strapi/design-system';
 import { useIntl } from 'react-intl';
+import {useRBAC} from '@strapi/admin/strapi-admin'
+import { ListViewPage, DocumentRBAC } from '@internal/shared';
 
-import { getTranslation } from '../utils/getTranslation';
 
 const HomePage = () => {
   const { formatMessage } = useIntl();
+  const PERMISSIONS = [
+    'plugin::content-manager.explorer.create',
+    'plugin::content-manager.explorer.read',
+    'plugin::content-manager.explorer.update',
+    'plugin::content-manager.explorer.delete',
+    'plugin::content-manager.explorer.publish',
+  ];
+  const model ='api::event.event'
+  // Fetch permissions for the Event model
+  const {
+    permissions = [],
+    isLoading,
+    error,
+  } = useRBAC(
+    PERMISSIONS.map((action) => ({
+      action,
+      subject: model,
+    }))
+  );
 
   return (
-    <Main>
-      <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
+    <Main padding={8}>
+      <Typography variant={"alpha"}>Membership list</Typography>
+      <DocumentRBAC permissions={permissions} model={model}>
+        <ListViewPage />
+      </DocumentRBAC>
+
     </Main>
   );
 };
