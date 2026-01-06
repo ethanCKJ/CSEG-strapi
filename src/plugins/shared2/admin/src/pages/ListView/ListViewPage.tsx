27c5bd3879f431066140398bd503951a713987b2
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-06 12:03:06 +0000

    Stable printing

--- src/plugins/shared2/admin/src/pages/ListView/ListViewPage.tsx
+++ src/plugins/shared2/admin/src/pages/ListView/ListViewPage.tsx
@@ -1,9 +1,24 @@
 import React, {useMemo} from "react";
-import {useQueryParams} from "@strapi/admin/strapi-admin";
+import {useQueryParams, useNotification, useAPIErrorHandler} from "@strapi/admin/strapi-admin";
 import {buildValidParams} from "../../utils/api";
 import {useGetAllDocumentsQuery} from "../../services/documents";
+import {getTranslation} from "../../utils/translations";
+import {useNavigate} from "react-router-dom";
+import { useIntl } from 'react-intl';
+import {stringify} from "qs";
+import {ListFieldLayout} from "@strapi/content-manager/strapi-admin";
+import {useDocumentLayout} from "../../hooks/useDocumentLayout";
+import {useDoc} from "../../hooks/useDocument";
 
 export const ListViewPage = () => {
+  const {toggleNotification} = useNotification();
+  const { _unstableFormatAPIError: formatAPIError } = useAPIErrorHandler(getTranslation);
+  const navigate = useNavigate();
+  const { formatMessage } = useIntl();
+  const [displayedHeaders, setDisplayedHeaders] = React.useState<ListFieldLayout[]>([]);
+  const {collectionType, model, schema} = useDoc();
+  const { list } = useDocumentLayout(model);
+
   const [{ query }] = useQueryParams<{
     plugins?: Record<string, unknown>;
     page?: string;
@@ -17,9 +32,94 @@ export const ListViewPage = () => {
   console.log('query params', query);
   const params = useMemo(() => buildValidParams(query), [query]);
   const { data, error, isLoading } = useGetAllDocumentsQuery({
-    model:'api::event.event',
+    model:model,
     params,
   });
+
+
+
+  /**
+   * If the API returns an error, display a notification
+   */
+  React.useEffect(() => {
+    if (error) {
+      toggleNotification({
+        type: 'danger',
+        message: formatAPIError(error),
+      });
+    }
+  }, [error, formatAPIError, toggleNotification]);
+
+  const { results = [], pagination } = data ?? {};
+
+  React.useEffect(() => {
+    if (pagination && pagination.pageCount > 0 && pagination.page > pagination.pageCount) {
+      navigate(
+          {
+            search: stringify({
+              ...query,
+              page: pagination.pageCount,
+            }),
+          },
+          { replace: true }
+      );
+    }
+  }, [pagination, formatMessage, query, navigate]);
+
+  // No need canCreate. Just set true.
+
+  /**
+   * Format the table headers without injecting additional columns.
+   */
+  const tableHeaders = React.useMemo(() => {
+    const headers = { displayedHeaders, layout: list };
+
+    const formattedHeaders = headers.displayedHeaders.map<ListFieldLayout>((header) => {
+      /**
+       * When the header label is a string, it is an attribute on the current content-type:
+       * Use the attribute name value to compute the translation.
+       * Otherwise, it should be a  translation object coming from a plugin that injects into the table (ie i18n, content-releases, review-workflows):
+       * Use the translation object as is.
+       */
+      const translation =
+          typeof header.label === 'string'
+              ? {
+                id: `content-manager.content-types.${model}.${header.name}`,
+                defaultMessage: header.label,
+              }
+              : header.label;
+
+      return {
+        ...header,
+        label: formatMessage(translation),
+        name: `${header.name}${header.mainField?.name ? `.${header.mainField.name}` : ''}`,
+      };
+    });
+
+    if (schema?.options?.draftAndPublish) {
+      formattedHeaders.push({
+        attribute: {
+          type: 'custom',
+        },
+        name: 'status',
+        label: formatMessage({
+          id: getTranslation(`containers.list.table-headers.status`),
+          defaultMessage: 'status',
+        }),
+        searchable: false,
+        sortable: false,
+      } satisfies ListFieldLayout);
+    }
+
+    return formattedHeaders;
+  }, [
+    displayedHeaders,
+    formatMessage,
+    list,
+    schema?.options?.draftAndPublish,
+    model,
+  ]);
+  console.log('tableHeaders', tableHeaders);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -29,5 +129,6 @@ export const ListViewPage = () => {
     return <div>Error loading documents</div>;
   }
   console.log('data', data, 'error', error, 'isLoading', isLoading);
+
   return (<div>List View Page</div>);
 }
