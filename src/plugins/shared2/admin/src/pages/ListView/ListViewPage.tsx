eb3d60a91b4f79e856422e7a6f758a3c4e5f1ca0
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-06 10:13:59 +0000

    Successfully printed events to terminal

--- src/plugins/shared2/admin/src/pages/ListView/ListViewPage.tsx
+++ src/plugins/shared2/admin/src/pages/ListView/ListViewPage.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useMemo} from "react";
+import {useQueryParams} from "@strapi/admin/strapi-admin";
+import {buildValidParams} from "../../utils/api";
+import {useGetAllDocumentsQuery} from "../../services/documents";
 
 export const ListViewPage = () => {
+  const [{ query }] = useQueryParams<{
+    plugins?: Record<string, unknown>;
+    page?: string;
+    pageSize?: string;
+    sort?: string;
+  }>({
+    page: '1',
+    pageSize: '10',
+    sort: 'id:ASC',
+  });
+  console.log('query params', query);
+  const params = useMemo(() => buildValidParams(query), [query]);
+  const { data, error, isLoading } = useGetAllDocumentsQuery({
+    model:'api::event.event',
+    params,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading documents</div>;
+  }
+  console.log('data', data, 'error', error, 'isLoading', isLoading);
   return (<div>List View Page</div>);
 }
