9bbae8c675afbff295946c08ff7d9d4d3eb05865
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-07 14:15:31 +0000

    Just before commenting out the tooltips

--- src/plugins/tester-plugin/admin/src/pages/HomePage.tsx
+++ src/plugins/tester-plugin/admin/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Main } from '@strapi/design-system';
+import {Main, Tooltip} from '@strapi/design-system';
 import { useIntl } from 'react-intl';
 
 import { getTranslation } from '../utils/getTranslation';
@@ -9,6 +9,9 @@ const HomePage = () => {
   return (
     <Main>
       <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
+      <Tooltip label="This is a tooltip">
+        <div>Hello and hover over me</div>
+      </Tooltip>
     </Main>
   );
 };
