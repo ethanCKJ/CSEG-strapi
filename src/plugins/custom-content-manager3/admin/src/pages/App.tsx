9927176783d6b7aa9fc84f4578f9d21ac098a875
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-10 12:32:08 +0000

    Resolved TooltipProvider error. Recalled the tester-plugin also required the DesignSystemProvider

--- src/plugins/custom-content-manager3/admin/src/pages/App.tsx
+++ src/plugins/custom-content-manager3/admin/src/pages/App.tsx
@@ -1,9 +1,11 @@
-import { Page } from '@strapi/strapi/admin';
+import { Page} from '@strapi/strapi/admin';
 import {Routes, Route, useParams} from 'react-router-dom';
+
 
 import { HomePage } from './HomePage';
 import {ProtectedListViewPage} from "./ListView/ListViewPage";
 import {COLLECTION_TYPES, SINGLE_TYPES} from "../constants/collections";
+import { DesignSystemProvider } from "@strapi/design-system";
 /**
  * On single collection types (e.g. about page), use the EditViewPage and
  * for collection types (e.g. events), use the ListViewPage.
@@ -23,11 +25,14 @@ const CollectionTypePages = () => {
 
 const App = () => {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path={"/:collectionType/:slug"} element={<CollectionTypePages />} />
-      <Route path="*" element={<Page.Error />} />
-    </Routes>
+    <DesignSystemProvider locale={"en-GB"}>
+      <Routes>
+        <Route index element={<HomePage/>}/>
+        <Route path={"/:collectionType/:slug"} element={<CollectionTypePages/>}/>
+        <Route path="*" element={<Page.Error/>}/>
+      </Routes>
+    </DesignSystemProvider>
+
   );
 };
 
