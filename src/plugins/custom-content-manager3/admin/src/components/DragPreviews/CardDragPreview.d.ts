5e6ced98fbad927fedc8237cba79088fd6a18d64
Ethan Cheam <ethan.cheam123@gmail.com>
2026-01-22 11:58:58 +0000

    Added drag previews

---
+++ src/plugins/custom-content-manager3/admin/src/components/DragPreviews/CardDragPreview.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+interface CardDragPreviewProps {
+    label: string;
+    isSibling?: boolean;
+}
+declare const CardDragPreview: ({ label, isSibling }: CardDragPreviewProps) => import("react").JSX.Element;
+export { CardDragPreview };
+export type { CardDragPreviewProps };
