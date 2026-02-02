61885161d2dcd7e10f15dbd474265ee3dbba6459
Ethan Cheam <ethan.cheam123@gmail.com>
2026-02-02 11:57:23 +0000

    Rebuilt cm

--- src/plugins/custom-content-manager3/dist/admin/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const index = require("../_chunks/index-DwujH_yg.js");
-require("prismjs");
-module.exports = index.index;
--- src/plugins/custom-content-manager3/dist/admin/index.mjs
+++
@@ -1,5 +0,0 @@
-import { i } from "../_chunks/index-C26PLF82.mjs";
-import "prismjs";
-export {
-  i as default
-};
