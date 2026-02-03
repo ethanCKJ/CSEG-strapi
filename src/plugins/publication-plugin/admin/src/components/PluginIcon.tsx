9b30e07bb9e1e8b3d5c30a4c1ec981c3b698a58e
Ethan Cheam <ethan.cheam123@gmail.com>
2026-02-03 11:06:46 +0000

    Returned text based icons

--- src/plugins/publication-plugin/admin/src/components/PluginIcon.tsx
+++ src/plugins/publication-plugin/admin/src/components/PluginIcon.tsx
@@ -1,10 +1,10 @@
 import { File } from '@strapi/icons';
 
 const PluginIcon = () =>
-  // <div style={{display: 'flex', alignItems: 'center', fontSize: '16px', gap:'8px'}}>
-  //   <File width={20} height={20}/>
-  //   <span>{"Publications"}</span>
-  // </div>;
-  <File width={20} height={20}/>
+  <div style={{display: 'flex', alignItems: 'center', fontSize: '16px', gap:'8px'}}>
+    <File width={20} height={20}/>
+    <span>{"Publications"}</span>
+  </div>;
+  // <File width={20} height={20}/>
 
 export { PluginIcon };
